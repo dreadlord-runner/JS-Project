5a3b171a3464c0fefed1d7b5996b447c2d64141e
dreadlord-runner <mayankyadav8285@gmail.com>
2026-02-28 22:41:50 +0530

    Refactors setup of localStorage spy in tests
    Moves the setup of the localStorage setItem spy to a beforeEach block to reduce code duplication and ensure consistent test initialization.

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -1,9 +1,11 @@
 import { addToCart, cart, loadFromStorage } from "../../data/cart.js";
 
 describe("tets suite: addToCart", () => {
-  it("adds an existing product to the cart", () => {
+  beforeEach(() => {
     spyOn(localStorage, "setItem");
+  });
 
+  it("adds an existing product to the cart", () => {
     spyOn(localStorage, "getItem").and.callFake(() => {
       return JSON.stringify([
         {
@@ -32,8 +34,6 @@ describe("tets suite: addToCart", () => {
   });
 
   it("adds a new product to the cart", () => {
-    spyOn(localStorage, "setItem");
-
     spyOn(localStorage, "getItem").and.callFake(() => {
       return JSON.stringify([]);
     });
